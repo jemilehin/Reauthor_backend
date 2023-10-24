9c6ff3a2be1e6262ca9347c786395b0353405eee
Temidev <temidayo@blockknowledge.co>
2023-10-24 10:12:16 +0100

    configured project structure

---
+++ dist/server.js
@@ -0,0 +1,8 @@
+"use strict";
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+const port = 4000;
+app.listen(port, () => console.log(`Sever is running on port--> ${port}`));
+//# sourceMappingURL=server.js.map
